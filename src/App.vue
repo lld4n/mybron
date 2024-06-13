780af47a2c2490b0ab0655ef53654a260dde18e8
lldan <safinildan@gmail.com>
2024-06-13 23:45:32 +0300

    тестируем темы телеграма

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="wrapper">
+    <div class="header">
+      <button class="header__btn">
+        <User />
+      </button>
+      <button class="header__btn">
+        <Settings />
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import User from "./assets/icons/user.svg";
+import Settings from "./assets/icons/settings.svg";
+</script>
+
+<style lang="scss" scoped>
+.wrapper {
+  display: flex;
+  flex-direction: column;
+}
+.header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 12px;
+  width: 100%;
+  &__btn {
+    width: 36px;
+    height: 36px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: var(--tg-theme-header-bg-color);
+    path {
+      fill: var(--tg-theme-text-color);
+    }
+  }
+}
+</style>
