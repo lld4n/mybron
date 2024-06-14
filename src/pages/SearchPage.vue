5493a3016adc77efd44650101a83154136997b44
lldan <safinildan@gmail.com>
2024-06-14 23:44:41 +0300

    смотрим на поведение кнопки назад

--- src/pages/SearchPage.vue
+++ src/pages/SearchPage.vue
@@ -1,8 +1,16 @@
 <script setup lang="ts">
-import { onMounted } from "vue";
-
+import { onMounted, onUnmounted } from "vue";
+import { useRouter } from "vue-router";
+const router = useRouter();
 onMounted(() => {
   window.Telegram.WebApp.BackButton.isVisible = true;
+  window.Telegram.WebApp.BackButton.onClick(() => {
+    router.push("/");
+  });
+});
+
+onUnmounted(() => {
+  window.Telegram.WebApp.BackButton.isVisible = false;
 });
 </script>
 
