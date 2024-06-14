54ccca089678848de2f7d6c8320fa645556768dd
lldan <safinildan@gmail.com>
2024-06-14 23:42:17 +0300

    тестим кнопку назад

--- src/pages/SearchPage.vue
+++ src/pages/SearchPage.vue
@@ -1,4 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted } from "vue";
+
+onMounted(() => {
+  window.Telegram.WebApp.BackButton.isVisible = true;
+});
+</script>
 
 <template>
   <router-link to="/">обратно</router-link>
